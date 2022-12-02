7257905b3d8ff04e2c52162fef8a59da162efb04
wsanon <sanon427@gmail.com>
2022-12-02 20:19:22 +0100

    Update swagger v

--- src/main/java/com/profileback/boardpersoback/Controleur/ProjectController.java
+++ src/main/java/com/profileback/boardpersoback/Controleur/ProjectController.java
@@ -2,12 +2,16 @@ package com.profileback.boardpersoback.Controleur;
 
 import com.profileback.boardpersoback.Model.Projet;
 import com.profileback.boardpersoback.Service.ProjetService.ProjetService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+
+@Api("API pour les opérations CRUD sur les projets.")
 
 @RestController
 @RequestMapping("/api/projet")
@@ -19,11 +23,13 @@ public class ProjectController {
         this.projetService = projetService;
     }
 
+    @ApiOperation(value = "Récupère la liste des projets !")
     @GetMapping("/list")
     public ResponseEntity<List<Projet>> getall(){
         return ResponseEntity.ok(projetService.getall());
     }
 
+    @ApiOperation(value = "Crée un nouveau projet !")
     @PostMapping("/save")
     public ResponseEntity<Projet> saveUser(@RequestBody Projet projet){
         URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("api/projet/save").toUriString());
